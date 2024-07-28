fd6cb0e5b93de1e0720ee38b4e9d92901a6b1ee9
gchikovnaii25 <gchikovanii25@gmail.com>
2024-07-28 21:51:00 +0400

    Spinner Implemented

--- EmphatyWave.Web/obj/Debug/net8.0/EmphatyWave.Web.AssemblyInfo.cs
+++ EmphatyWave.Web/obj/Debug/net8.0/EmphatyWave.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmphatyWave.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9b179aec857e71f6dccca08da007a4238ba9c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631336a0f8915f4dba592aa1f0d4f27fa008286b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmphatyWave.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmphatyWave.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
